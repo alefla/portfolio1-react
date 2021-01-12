513503fa983845e9bbaa96c7f1a022b3b99918a8
Kris Celone <kriscelone@Kriss-MacBook-Pro.local>
2021-01-12 12:00:53 -0800

    added complete header

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Typed from 'react-typed';
+
+
+const Header = () => {
+    return (
+        <div className="header-wrapper">
+            <div className="main-info">
+                <h1>web development & website promotions</h1>
+                <Typed className="typed-text" strings={["Web Design", "Web Development", "Facebook Ads SMM", "Google Ads"]} typeSpeed={70} backspeed={60} loop />
+                <a href="#" className="btn-main-offer">contact me</a>
+                
+            </div> 
+            
+        </div>
+        
+        
+        
+        
+    )
+}
+export default Header;
